d18f0b0de3e7c99adff257052b531e911b7fb64c
Greg Depoire--Ferrer <greg.depoire@gmail.com>
2020-10-26 01:56:40 +0100

    Turn the 1-pixel cursor into a 11x11 rectangle for better visibility

--- example-app/src/main.c
+++ example-app/src/main.c
@@ -3,8 +3,8 @@
 
 #include <assert.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
@@ -26,12 +26,11 @@ static struct xdg_wm_base *wm_base = NULL;
 
 /* Surface variables. */
 static struct wl_surface *surface;
-static struct wl_buffer *surface_buf;
+static struct wl_buffer *surface_buffer;
 static uint32_t *pixels;
 
 /* Cursor variables. */
-static struct wl_cursor_image *def_cursor_image;
-static struct wl_surface *def_cursor_surface;
+static struct wl_surface *cursor_surface;
 
 static bool running = true;
 
@@ -48,48 +47,77 @@ static bool requested_frame = false;
 static int frame_cursor_x;
 static int frame_cursor_y;
 
-static void sw_cursor_move(struct sw_cursor *cursor, int new_x, int new_y, uint32_t mask)
+static void clamp(int *val, int min, int max)
+{
+    if (*val < min) {
+        *val = min;
+    } else if (*val > max) {
+        *val = max;
+    }
+}
+
+static void xor_rect(int x_min, int y_min, int x_max, int y_max, uint32_t mask)
+{
+    clamp(&x_min, 0, width);
+    clamp(&y_min, 0, height);
+    clamp(&x_max, 0, width);
+    clamp(&y_max, 0, height);
+
+    if (x_min >= x_max || y_min >= y_max)
+        return;
+
+    for (int y = y_min; y <= y_max; ++y) {
+        for (int x = x_min; x <= x_max; ++x) {
+            pixels[y * width + x] ^= mask;
+        }
+    }
+
+    wl_surface_damage_buffer(surface, x_min, y_min, x_max - x_min + 1,
+                             y_max - y_min + 1);
+}
+
+static void sw_cursor_move(struct sw_cursor *cursor, int new_x, int new_y,
+                           uint32_t mask)
 {
     if (cursor->is_visible) {
         if (new_x == cursor->curr_x && new_y == cursor->curr_y)
             return;
 
-        /* Erase the pixel at the old location. */
-        pixels[(cursor->curr_y * width) + cursor->curr_x] &= ~mask;
-        wl_surface_damage(surface, cursor->curr_x, cursor->curr_y, 1, 1);
+        /* Erase the rectangle at the old location. */
+        xor_rect(cursor->curr_x - 5, cursor->curr_y - 5, cursor->curr_x + 5,
+                 cursor->curr_y + 5, mask);
     }
 
-    /* Draw a pixel at the new location */
-    pixels[(new_y * width) + new_x] |= mask;
-    wl_surface_damage(surface, new_x, new_y, 1, 1);
+    /* Draw a rectangle at the new location. */
+    xor_rect(new_x - 5, new_y - 5, new_x + 5, new_y + 5, mask);
 
     cursor->curr_x = new_x;
     cursor->curr_y = new_y;
     cursor->is_visible = true;
 
     /* Display the new buffer. */
-    wl_surface_attach(surface, surface_buf, 0, 0);
+    wl_surface_attach(surface, surface_buffer, 0, 0);
     wl_surface_commit(surface);
 }
 
 static void global_add_handler(void *data, struct wl_registry *registry,
-                       uint32_t name, const char *interface, uint32_t version)
+                               uint32_t name, const char *interface,
+                               uint32_t version)
 {
     if (!strcmp(interface, "wl_compositor")) {
         compositor =
-            wl_registry_bind(registry, name, &wl_compositor_interface, 3);
+            wl_registry_bind(registry, name, &wl_compositor_interface, 4);
     } else if (!strcmp(interface, "wl_shm")) {
         shm = wl_registry_bind(registry, name, &wl_shm_interface, 1);
     } else if (!strcmp(interface, "wl_seat")) {
         seat = wl_registry_bind(registry, name, &wl_seat_interface, 1);
     } else if (!strcmp(interface, "xdg_wm_base")) {
-        wm_base =
-            wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
+        wm_base = wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
     }
 }
 
 static void global_remove_handler(void *data, struct wl_registry *registry,
-                          uint32_t name)
+                                  uint32_t name)
 {
 }
 
@@ -98,7 +126,9 @@ static const struct wl_registry_listener registry_listener = {
     .global_remove = global_remove_handler,
 };
 
-static void frame_done_handler(void *data, struct wl_callback *callback, uint32_t callback_data) {
+static void frame_done_handler(void *data, struct wl_callback *callback,
+                               uint32_t callback_data)
+{
     sw_cursor_move(&frame_cursor, frame_cursor_x, frame_cursor_y, 0x0000FF00);
     requested_frame = false;
 }
@@ -108,45 +138,46 @@ static const struct wl_callback_listener frame_listener = {
 };
 
 static void pointer_enter_handler(void *data, struct wl_pointer *pointer,
-                           uint32_t serial, struct wl_surface *surface,
-                           wl_fixed_t x, wl_fixed_t y)
+                                  uint32_t serial, struct wl_surface *surface,
+                                  wl_fixed_t x, wl_fixed_t y)
 {
-    wl_pointer_set_cursor(pointer, serial, def_cursor_surface,
-                          def_cursor_image->hotspot_x, def_cursor_image->hotspot_y);
+    wl_pointer_set_cursor(pointer, serial, cursor_surface, 5, 5);
 }
 
 static void pointer_leave_handler(void *data, struct wl_pointer *pointer,
-                           uint32_t serial, struct wl_surface *surface)
+                                  uint32_t serial, struct wl_surface *surface)
 {
 }
 
 static void pointer_motion_handler(void *data, struct wl_pointer *pointer,
-                            uint32_t time, wl_fixed_t surface_x, wl_fixed_t surface_y)
+                                   uint32_t time, wl_fixed_t surface_x,
+                                   wl_fixed_t surface_y)
 {
     int int_x = wl_fixed_to_int(surface_x);
     int int_y = wl_fixed_to_int(surface_y);
 
     /* Update immediate cursor directly. */
-    sw_cursor_move(&immediate_cursor, int_x, int_y, 0x0000FF);   
+    sw_cursor_move(&immediate_cursor, int_x, int_y, 0x00FF0000);
 
     /* Request a frame notification to update frame cursor. */
+    frame_cursor_x = int_x;
+    frame_cursor_y = int_y;
     if (!requested_frame) {
         struct wl_callback *frame_callback = wl_surface_frame(surface);
         wl_callback_add_listener(frame_callback, &frame_listener, NULL);
+        wl_surface_commit(surface);
         requested_frame = true;
     }
-    frame_cursor_x = int_x;
-    frame_cursor_y = int_y;
 }
 
 static void pointer_button_handler(void *data, struct wl_pointer *pointer,
-                            uint32_t serial, uint32_t time, uint32_t button,
-                            uint32_t state)
+                                   uint32_t serial, uint32_t time,
+                                   uint32_t button, uint32_t state)
 {
 }
 
-static void pointer_axis_handler(void *data, struct wl_pointer *pointer, uint32_t time,
-                          uint32_t axis, wl_fixed_t value)
+static void pointer_axis_handler(void *data, struct wl_pointer *pointer,
+                                 uint32_t time, uint32_t axis, wl_fixed_t value)
 {
 }
 
@@ -158,8 +189,8 @@ const struct wl_pointer_listener pointer_listener = {
     .axis = pointer_axis_handler};
 
 static void xdg_surface_configure_handler(void *data,
-                                   struct xdg_surface *xdg_surface,
-                                   uint32_t serial)
+                                          struct xdg_surface *xdg_surface,
+                                          uint32_t serial)
 {
     xdg_surface_ack_configure(xdg_surface, serial);
 }
@@ -167,9 +198,10 @@ static void xdg_surface_configure_handler(void *data,
 const struct xdg_surface_listener xdg_surface_listener = {
     .configure = xdg_surface_configure_handler};
 
-static void xdg_toplevel_configure_handler(
-    void *data, struct xdg_toplevel *xdg_toplevel, int32_t width,
-    int32_t height, struct wl_array *states)
+static void xdg_toplevel_configure_handler(void *data,
+                                           struct xdg_toplevel *xdg_toplevel,
+                                           int32_t width, int32_t height,
+                                           struct wl_array *states)
 {
 }
 
@@ -196,17 +228,23 @@ int main(int argc, char **argv)
     assert(seat);
     assert(wm_base);
 
-    /* Load default cursor. */
-    struct wl_cursor_theme *cursor_theme = wl_cursor_theme_load(NULL, 24, shm);
-    struct wl_cursor *def_cursor =
-        wl_cursor_theme_get_cursor(cursor_theme, "left_ptr");
-    def_cursor_image = def_cursor->images[0];
+    /* Create a shared memory pool for the surface's buffer and for the
+     * cursor's surface's buffer. */
+    int memfd_size = (width * height + 11 * 11) * 4;
+    int memfd = memfd_create("buffer", 0);
+    assert(memfd != -1);
+    assert(ftruncate(memfd, memfd_size) != -1);
+    pixels = mmap(NULL, memfd_size, PROT_WRITE, MAP_SHARED, memfd, 0);
+    struct wl_shm_pool *pool = wl_shm_create_pool(shm, memfd, memfd_size);
 
-    /* Create surface for default cursor. */
-    def_cursor_surface = wl_compositor_create_surface(compositor);
-    wl_surface_attach(def_cursor_surface,
-                      wl_cursor_image_get_buffer(def_cursor_image), 0, 0);
-    wl_surface_commit(def_cursor_surface);
+    /* Create a buffer and a surface for the cursor. */
+    for (int i = 0; i < 11 * 11; ++i)
+        pixels[height * width + i] = 0x00FFFFFF;
+    struct wl_buffer *cursor_buffer = wl_shm_pool_create_buffer(
+        pool, height * width * 4, 11, 11, 11 * 4, WL_SHM_FORMAT_XRGB8888);
+    cursor_surface = wl_compositor_create_surface(compositor);
+    wl_surface_attach(cursor_surface, cursor_buffer, 0, 0);
+    wl_surface_commit(cursor_surface);
 
     /* Setup pointer. */
     struct wl_pointer *pointer = wl_seat_get_pointer(seat);
@@ -224,18 +262,10 @@ int main(int argc, char **argv)
     wl_surface_commit(surface);
     wl_display_roundtrip(display);
 
-    /* Create a buffer for the surface. */
-    int memfd_size = width * height * 4;
-    int memfd = memfd_create("buffer", 0);
-    assert(memfd != -1);
-    assert(ftruncate(memfd, memfd_size) != -1);
-    pixels = mmap(NULL, memfd_size, PROT_WRITE, MAP_SHARED, memfd, 0);
-    struct wl_shm_pool *pool = wl_shm_create_pool(shm, memfd, memfd_size);
-    surface_buf = wl_shm_pool_create_buffer(
+    /* Create a buffer and attach it to the surface. */
+    surface_buffer = wl_shm_pool_create_buffer(
         pool, 0, width, height, width * 4, WL_SHM_FORMAT_XRGB8888);
-
-    /* Attach the buffer to the surface. */
-    wl_surface_attach(surface, surface_buf, 0, 0);
+    wl_surface_attach(surface, surface_buffer, 0, 0);
     wl_surface_commit(surface);
 
     /* Main loop: dispatch events. */
